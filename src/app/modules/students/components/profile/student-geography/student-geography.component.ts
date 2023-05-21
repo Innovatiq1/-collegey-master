08ffbd96a87ec7285164ba1293607562b4c5c0c0
Rasagna06 <rasagna.pinninti@innovatiqconsulting.com>
2023-05-21 17:25:18 +0530

    save and complete later fixed

--- src/app/modules/students/components/profile/student-geography/student-geography.component.ts
+++ src/app/modules/students/components/profile/student-geography/student-geography.component.ts
@@ -276,25 +276,6 @@ export class StudentGeographyComponent implements OnInit, OnDestroy {
       this.onSubmitGeographyForm.emit(geographyFormData);
       this.commonService.sendUpdate(outputObj);
     }
-    }
-    else {
-      this.studentService.redirectToDashboard(exit);
-
-
-      let geographyFormData = this.geographyFormGroup.getRawValue();
-
-
-      let outputObj = {
-        city: data?.geography?.city == 11111111111 ? 0 : data?.geography?.city,
-        state: data?.geography?.state,
-        country: data?.geography?.country,
-      }
-
-
-      geographyFormData.geography.redirectAction = exit;
-      this.onSubmitGeographyForm.emit(geographyFormData);
-      this.commonService.sendUpdate(outputObj);
-    }
   }else {
       this.studentService.redirectToDashboard(exit);
 
